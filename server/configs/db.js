99b564413f64d0cc1e69f2afa49cf06efed15a5d
Alok Kumar Das <alokhacs222729@gmail.com>
2025-10-04 18:32:10 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,9 +4,16 @@ const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('Database is connected'));
 
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickreserve`)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "cinegharshow",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("Connected DB:", mongoose.connection.name);
     } catch (error) {
-        console.logo(error.message);
-    }
+        console.log("MongoDB connection error:", error.message);
+    } 
 }
+
 export default connectDB;
